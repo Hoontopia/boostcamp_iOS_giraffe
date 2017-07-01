b712f0d4a937353896055381f879c84e35be177c
임성훈 <CodeFactory@imseonghun-ui-MacBook-Pro.local>
2017-07-01 13:27:09 +0900

    Add a Quiz App

---
+++ week1/Quiz/Quiz/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 임성훈 on 2017. 7. 1..
+//  Copyright © 2017년 임성훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    /// Xcode에게 이 아웃렛이 인터페이스 빌더를 사용하여 라벨 객체와 연결한다는 것을 알린다.
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["From what is cognac made?",
+                               "What is 7+7?",
+                               "What is the capital of Vermont?"]
+    let answers: [String] = ["Grapes",
+                            "14",
+                            "Montpelier"]
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    @IBAction func showNextQuestion(sender: AnyObject) {
+        /// ++currentQuestionIndex에서 문법 중 증감연산자 삭제로 인한 변경
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+}
+
