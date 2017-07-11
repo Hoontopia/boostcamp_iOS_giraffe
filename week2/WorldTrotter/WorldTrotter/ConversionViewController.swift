3b324a01a9247dafcaa4f0a0626b3b22b3887128
임성훈 <CodeFactory@imseonghun-ui-MacBook-Pro.local>
2017-07-12 00:55:22 +0900

    Considering extendability of background color

--- week2/WorldTrotter/WorldTrotter/ConversionViewController.swift
+++ week2/WorldTrotter/WorldTrotter/ConversionViewController.swift
@@ -8,29 +8,45 @@
 
 import UIKit
 
+/*
+ 배경색이 10개 이상이라면?
 struct BackgroundColor {
     static let lightColor: UIColor = UIColor(red: 245/255, green: 244/255, blue: 241/255, alpha: 1.0)
     static let darkColor: UIColor = UIColor(red: 50/255, green: 100/255, blue: 0, alpha: 1.0)
+
+} */
+
+struct BackgroundColor {
+    let colorArray: [UIColor]
+    let lightColor: UIColor = UIColor(red: 245/255, green: 244/255, blue: 241/255, alpha: 1.0)
+    let darkColor: UIColor = UIColor(red: 50/255, green: 100/255, blue: 0, alpha: 1.0)
+    var colorIndex: Int = -1 // getNextColor 첫 호출시 0으로 변경
+    
+    init() {
+        colorArray = [lightColor, darkColor]
+    }
+    
+    // 다음 컬러 반환
+    mutating func getNextColor() -> UIColor {
+        colorIndex = (colorIndex + 1) % colorArray.count
+        return colorArray[colorIndex]
+    }
 }
 
 class ConversionViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var celsiusLabel: UILabel!
     @IBOutlet var textField: UITextField!
+    var backgroundColor: BackgroundColor = BackgroundColor()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("ConversionViewController loaded its view.")
     }
-  
+    
     override func viewWillAppear(_ animated: Bool) {
-        if self.view.backgroundColor == BackgroundColor.lightColor {
-            view.backgroundColor = BackgroundColor.darkColor
-        }
-        else {
-            self.view.backgroundColor = BackgroundColor.lightColor
-        }
+        self.view.backgroundColor = backgroundColor.getNextColor()
     }
     
     /// 사용자가 숫자를 입력하지 않을 수도 있으므로 옵셔널로 선언
@@ -41,8 +57,8 @@ class ConversionViewController: UIViewController, UITextFieldDelegate {
     }
     
     var celsiusValue: Double? {
-        if let value = fahrenheitValue {
-            return (value - 32) * (5/9)
+        if let fahrenheitValue = fahrenheitValue {
+            return (fahrenheitValue - 32) * (5/9)
         }
         else {
             return nil
