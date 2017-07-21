6c7a8c299e0e0db3f513a9c3b91022986beeba0e
Limsunghun <limsung1541@naver.com>
2017-07-21 18:22:50 +0900

    Init a BoardApplication Project

---
+++ AddtionalProject/BoardApplication/BoardApplication/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BoardApplication
+//
+//  Created by 임성훈 on 2017. 7. 21..
+//  Copyright © 2017년 임성훈. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
